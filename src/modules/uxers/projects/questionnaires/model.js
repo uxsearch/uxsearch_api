b919643bc32f82b19ee05684a1f96ab10fc191da
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-08-05 13:38:20 +0700

    [UPDATE] develop : update getNote function

--- src/modules/uxers/projects/questionnaires/model.js
+++ src/modules/uxers/projects/questionnaires/model.js
@@ -1,35 +1,42 @@
 import firebase from 'api/firebase-config'
 import { db } from 'api/firebasehelper'
+import { getOptions } from './options/model'
 
 const collectionUxer = 'uxers'
 const collectionProject = 'projects'
 const collectionQuestionnaire = 'questionnaires'
 
-async function getQuestionnaire (uxerId, projectId) {
+async function getQuestionnaire(uxerId, projectId) {
   const ref = await db.collection(collectionUxer).doc(uxerId)
     .collection(collectionProject).doc(projectId)
     .collection(collectionQuestionnaire).where('type_question', '==', 'questionnaire').get()
   let questionnaires = []
-  ref.forEach(sanpshot => {
+  ref.forEach(snapshot => {
     questionnaires = [...questionnaires, {
-      id: sanpshot.id,
-      data: sanpshot.data()
+      id: snapshot.id,
+      data: snapshot.data()
     }]
   })
   return questionnaires
 }
 
-async function getNote (uxerId, projectId) {
+async function getNote(uxerId, projectId) {
+  let notes = []
   const ref = await db.collection(collectionUxer).doc(uxerId)
     .collection(collectionProject).doc(projectId)
     .collection(collectionQuestionnaire).where('type_question', '==', 'note').get()
-  let notes = []
-  ref.forEach(sanpshot => {
-    notes = [...notes, {
-      id: sanpshot.id,
-      data: sanpshot.data()
-    }]
+  ref.forEach(async snapshot => {
+    const options = await getOptions(uxerId, projectId, snapshot.id)
+    notes.push({
+      id: snapshot.id,
+      data: {
+        question: snapshot.data(),
+        options,
+      }
+    })
+    console.log('loop: ', notes)
   })
+  console.log('end: ', notes)
   return notes
 }
 
