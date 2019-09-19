539ab3cee93b08f2e1dd114c634718fcb121cf7a
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-09-19 16:32:15 +0700

    [UPDATE] develop : add multer to server for collect file

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import multer from 'multer'
 
 import router from 'api/routers'
 
@@ -12,17 +13,22 @@ app.use(function (req, res, next) {
   next()
 });
 
-app.listen(port, function () {
-  console.log('Ready on http://localhost:', port)
-})
 
 app.get('/', (req, res) => {
   res.send('#UX SEARCH API is RUNNING !')
 })
 
+var upload = multer()
 app.use(bodyParser.json())
-app.use('/api', router)
+// app.use(bodyParser.urlencoded({ extended: true }))
 
-app.use(bodyParser.urlencoded({ extended: false }))
+app.use('/api',upload.fields([{ name: 'file'}]), router)
+
+global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
+app.listen(port, function () {
+  console.log('Ready on http://localhost:', port)
+})
+// app.use(bodyParser.urlencoded({ extended: false }))
 
 export default app
