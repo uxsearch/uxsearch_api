d7396425eac87f9d4ab428bc0496a22c196a2508
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-10-14 15:36:18 +0700

    [UPDATE] develop : delete some function for unused in server.js

--- server.js
+++ server.js
@@ -8,11 +8,6 @@ import router from 'api/routers'
 const port = parseInt(process.env.PORT, 10) || 3000
 
 var app = express();
-// app.use(function (req, res, next) {
-//   res.header('Access-Control-Allow-Origin', '*')
-//   res.header('Access-Control-Allow-Headers', '*')
-//   next()
-// });
 app.use(cors())
 
 app.get('/', (req, res) => {
@@ -21,7 +16,6 @@ app.get('/', (req, res) => {
 
 var upload = multer()
 app.use(bodyParser.json())
-// app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api',upload.fields([{ name: 'file'}]), router)
 
@@ -30,6 +24,5 @@ global.XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 app.listen(port, function () {
   console.log('Ready on http://localhost:', port)
 })
-// app.use(bodyParser.urlencoded({ extended: false }))
 
 export default app
