ac6c03bebabe5e877b3b1029fd41adba57c5188e
Wasin Wachirapusitanun <wasin.wj@mail.kmutt.ac.th>
2019-10-25 15:13:03 +0700

    [FIX] develop : update condition of submit answer

--- src/modules/uxers/projects/experimenters/answerNote/model.js
+++ src/modules/uxers/projects/experimenters/answerNote/model.js
@@ -25,7 +25,7 @@ async function createAnswer(uxerId, projectId, experimenterId, valueAnswer) {
   const created_at = new Date()
   const updated_at = new Date()
   const { question_key, answer } = valueAnswer
-  
+
   await db.collection(collectionUxer).doc(uxerId)
     .collection(collectionProject).doc(projectId)
     .collection(collectionExperimenter).doc(experimenterId)
@@ -36,14 +36,14 @@ async function updateAnswer(uxerId, projectId, experimenterId, answers) {
   for (var i = 0; i < answers.length; i++) {
     const updated_at = new Date()
     const { answerId, answer } = answers[i]
-    if (answerId !== undefined) {
+    if (answerId === '' || answerId === undefined) {
+      await createAnswer(uxerId, projectId, experimenterId, answers[i])
+    } else if (answerId !== undefined) {
       await db.collection(collectionUxer).doc(uxerId)
         .collection(collectionProject).doc(projectId)
         .collection(collectionExperimenter).doc(experimenterId)
         .collection(collectionAnswer).doc(answerId)
         .set({ answer, updated_at }, { merge: true })
-    } else if (answerId === undefined) {
-      await createAnswer(uxerId, projectId, experimenterId, answers[i])
     }
   }
 }
